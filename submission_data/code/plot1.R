bd2cb57a7a9598fc3daf83806bf8da4980eea155
Enis Becirbegovic <enisbe@gmail.com>
2015-01-11 15:56:39 -0700

    another submission

--- submission_data/code/plot1.R
+++ submission_data/code/plot1.R
@@ -1,7 +1,10 @@
-setwd("X:/WD/Documents/Projects/Education/DataScience/DataScience/4_Exploratory_Data_Analysis/")
-setwd("Assignement_1")
+# set data file location
+dataFile = "X:/WD/Documents/Projects/Education/DataScience/DataScience/4_Exploratory_Data_Analysis/exdata-data-household_power_consumption/household_power_consumption.txt"
 
+#set wd to be where you will save you files.Specific to your local enviroment
+setwd ("X:/WD/Documents/Projects/Education/DataScience/DataScience/4_Exploratory_Data_Analysis/Assignement_1/ExData_Plotting1/submission_data")
 
+  
 cols =  c("Date", 
           "Time", 
           "Global_active_power", 
@@ -11,7 +14,7 @@ cols =  c("Date",
           "Sub_metering_2" , 
           "Sub_metering_3")
 
-df = read.csv("exdata-data-household_power_consumption/household_power_consumption.txt", 
+df = read.csv(dataFile, 
               sep=';', 
               nrows =2880, 
               header = TRUE, 
@@ -22,9 +25,12 @@ df$Date.Time = paste(df$Date, df$Time, sep= " ")
 
 df$Date.Time = strptime(df$Date.Time, "%d/%m/%Y %H:%M:%S")
 
+
+df = df[1:nrow(df)-1,] # deleted the last row b/c it goes into the next day. Row needed for labeling
+
 #Create plot 1
 
-png(file='plot1.png', 
+png(file='graphs/plot1.png', 
     bg = "transparent", 
     width=480, 
     height=480)
